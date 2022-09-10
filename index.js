2ed50d1dcffbd78803287d280a2a6b61ff9cec29
Ihor <107474617+IhorOzerov@users.noreply.github.com>
2022-09-10 23:50:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -64,73 +64,8 @@ let Gangster = new Cat("Gangster", "male", "Yohanna and Vanessa");
 let Gregory = new Man("Gregory", "Vanessa and Bandit");
 let Yohanna = new Woman("Yohanna", "Gangster and Gregory");
 let Bandit = new Dog('Bandit', 'male', "Gregory and Gangster");
-let Vanessa = new CatWoman("Vanessa", "Gangster and Yohanna")
+let Vanessa = new CatWoman("Vanessa", "Gangster and Yohanna");
 
-const members = [Gangster, Bandit, Gregory, Yohanna, Vanessa]
+const members = [Gangster, Bandit, Gregory, Yohanna, Vanessa];
 
-members.map(member => print(member.inhabitantSpeech()))
-
-/*
-const man = {
-   creature: 'human',
-   name: "Gregory",
-   gender: 'male',
-   hands: 2,
-   legs: 2,
-   say: "Hello, woman!",
-   friends: ["Bandit", "Gangster"]
-};
-const woman = {
-   creature: 'human',
-   name: "Yohanna",
-   gender: 'female',
-   hands: 2,
-   legs: 2,
-   say: "Hello, man!",
-   friends:"Gangster"
-};
-const dog = {
-   creature: 'dog',
-   name: "Bandit",
-   gender: 'male',
-   hands: 0,
-   legs: 4,
-   say: "Bark!",
-   friends:  ["Gregory", "Yohanna"]
-};
-const cat = {
-   creature: 'cat',
-   name: "Gangster",
-   gender: 'male',
-   hands: 0,
-   legs: 4,
-   say: "Where is my money, Gregory?!",
-   friends:  ["Gregory", "Vanessa"]
-};
-const catWoman = Object.create(cat);
-catWoman.creature = "cat-woman";
-catWoman.name = "Vanessa";
-catWoman.gender = 'female';
-catWoman.legs = 2;
-catWoman.hands = 2;
-catWoman.friends =  ["Yohanna", "Gangster"];
-
-const keys = {
-   creature: "I am ",
-   name: "My name is ",
-   gender: "I am a pretty ",
-   hands: "I have a ",
-   legs: "hands and ",
-   friends: "legs I like to talk with ",
-   say: "I would like to say "
-}
-
-const members = [man, woman, cat, dog, catWoman];
-const memberProps = ['creature', 'name', 'gender', 'hands', 'legs', 'friends', 'say'];
-members.forEach(member => 
-   print((memberProps 
-      .map(key => keys[key] + member[key]))
-      .join(" ")
-   )
-);
-*/
+members.map(member => print(member.inhabitantSpeech()));
